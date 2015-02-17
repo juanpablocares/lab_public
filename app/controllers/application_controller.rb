4c37e9a21abd5a1e46f0353d3192f3534695867a
Carlos Benner <aideas.cl>
2015-02-17 16:59:52 -0300

    cambiada ruta default y creacion de action 'angular' en applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,9 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+  
+  def angular
+  	
+  end
 end
