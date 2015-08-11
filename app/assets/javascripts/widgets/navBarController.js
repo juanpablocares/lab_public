e09aea74ee8bebb5fa421b6cbc867fe0df2415b7
jcares <juanpablo.cares@gmail.com>
2015-08-11 19:30:20 -0300

    cambios menores en ficha y primera version del nuevo menu que se debe mejorar

--- app/assets/javascripts/widgets/navBarController.js
+++ app/assets/javascripts/widgets/navBarController.js
@@ -1,4 +1,38 @@
-angular.module('lab').controller('NavBarController', function($rootScope, $state, $scope, $auth, $stateParams, Pacientes) {
+angular.module('lab').controller('NavBarController', function($rootScope, $state, $scope, $auth, $stateParams, $aside, Pacientes) {
+
+
+	$scope.asideState = {
+      open: false
+    };
+    
+    $scope.openAside = function(position, backdrop) {
+	  console.log(position);
+      $scope.asideState = {
+        open: true,
+        position: position
+      };
+      
+      function postClose() {
+        $scope.asideState.open = false;
+      }
+
+      $aside.open({
+        templateUrl: 'aside.html',
+        placement: position,
+        size: 'sm',
+        backdrop: backdrop,
+        controller: function($scope, $modalInstance) {
+          $scope.ok = function(e) {
+            $modalInstance.close();
+            e.stopPropagation();
+          };
+          $scope.cancel = function(e) {
+            $modalInstance.dismiss();
+            e.stopPropagation();
+          };
+        }
+      }).result.then(postClose, postClose);
+    }
 
 	$scope.searchForm = {};
 	$scope.paciente = {};
