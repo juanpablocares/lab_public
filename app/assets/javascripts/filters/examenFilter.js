99eeba8089b1d36566ea54a3c81114c0cb3ca0bf
jcares <juanpablo.cares@gmail.com>
2016-05-19 20:40:27 -0300

    arreglado error en busqueda de examenes en ficha

--- app/assets/javascripts/filters/examenFilter.js
+++ app/assets/javascripts/filters/examenFilter.js
@@ -8,11 +8,13 @@ angular.module('lab').filter('examenFilter', [function($filter) {
 		texto = texto.split(' ');
 		angular.forEach(inputArray, function(item){
 			var codigo = '';
+			var codigo_fonasa = '';
 			if(angular.isDefined(item.codigo) || item.codigo != ''){
 				codigo = codigo + item.codigo;
 			}
 			if(angular.isDefined(item.codigo_fonasa) || item.codigo_fonasa != ''){
 				codigo = codigo + ' - '+item.codigo_fonasa;
+				codigo_fonasa += item.codigo_fonasa;
 			}
 			codigo = codigo.toLowerCase();
 			var coincide = true;
@@ -23,7 +25,7 @@ angular.module('lab').filter('examenFilter', [function($filter) {
 				{
 					if((item.nombre != null && item.nombre != '' && item.nombre.toLowerCase().indexOf(valor) == 0 ) ||
 					   (item.nombre_impreso != null && item.nombre_impreso != '' && item.nombre_impreso.toLowerCase().indexOf(valor) == 0) ||
-					   (codigo.toLowerCase().indexOf(valor) == 0) ||
+					   (codigo.toLowerCase().indexOf(valor) == 0) || (codigo_fonasa.toLowerCase().indexOf(valor) == 0) ||
 					   (item.sigla != null && item.sigla != '' && item.sigla.toLowerCase().indexOf(valor) == 0))
 					{
 						//Coincide
